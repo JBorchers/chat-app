c1db1cf42225c9d9d6f76034540a3e8688f9728b
JBorchers <79878454+JBorchers@users.noreply.github.com>
2021-09-03 11:49:02 -0600

    created example code to display static messages using GiftedChat

--- components/Chat.js
+++ components/Chat.js
@@ -1,7 +1,43 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat'
 
 export default class Chat extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+    }
+  }
+
+  componentDidMount() {
+    // const name = this.props.route.params.username;
+    // this.props.navigation.setOptions({ title: name });
+    this.setState({
+      // each message requires an ID, a creation date, and a user object
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello developer',
+          createdAt: new Date(),
+          // user objects require a user ID, name, and avatar
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+        },
+      ],
+    });
+  }
+
+  onSend(messages = []) {
+   this.setState(previousState => ({
+    //  appends the new message to the messages object (to be displayed in the chat)
+     messages: GiftedChat.append(previousState.messages, messages),
+   }))
+ }
+
 
   render() {
     let name = this.props.route.params.name; // OR ...
@@ -10,9 +46,16 @@ export default class Chat extends React.Component {
     this.props.navigation.setOptions({ title: name });
 
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: this.props.route.params.bgColor }}>
-        <Text style={{ color: 'white', fontWeight: 'bold' }}>Welcome</Text>
-      </View>
+      // <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: this.props.route.params.bgColor }}>
+      //   <Text style={{ color: 'white', fontWeight: 'bold' }}>Welcome!</Text>
+      // </View>
+      <GiftedChat
+        messages={this.state.messages}
+        onSend={messages => this.onSend(messages)}
+        user={{
+          _id: 1,
+        }}
+      />
     );
   };
 }
