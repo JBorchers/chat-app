861422f4734c9832769e171164b281ef4591877a
JBorchers <79878454+JBorchers@users.noreply.github.com>
2021-09-22 12:59:55 -0600

    added functions to allow users to send/take photos, and send location

--- components/Chat.js
+++ components/Chat.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { View, Text, KeyboardAvoidingView, Platform, StyleSheet, Button } from 'react-native';
 import { GiftedChat, Bubble, InputToolbar } from 'react-native-gifted-chat';
+import MapView from "react-native-maps";
+import CustomActions from './CustomActions';
 
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
@@ -24,7 +26,6 @@ const firebaseConfig = {
 export default class Chat extends React.Component {
   constructor() {
     super();
-
     this.state = {
       messages: [],
       uid: 0,
@@ -33,26 +34,29 @@ export default class Chat extends React.Component {
         _id: "",
         name: "",
       },
+      isConnected: false,
+      image: null,
+      location: null
     };
-
+    
     // initializes the app
     if (!firebase.apps.length){
       firebase.initializeApp(firebaseConfig);
     }
-
+    
     // references the messages collection from firebase DB
     // stores and retrieves the chat messages the users send
     this.referenceChatMessages = firebase.firestore().collection("messages");
-
+    
   };
-
+  
   async getMessages() {
     let messages = '';
     let uid = '';
     try {
       messages = await AsyncStorage.getItem('messages') || [];
       uid = await AsyncStorage.getItem('uid');
-
+      
       this.setState({
         messages: JSON.parse(messages),
         uid: JSON.parse(uid),
@@ -61,7 +65,7 @@ export default class Chat extends React.Component {
       console.log(error.message);
     }
   };
-
+  
   async saveMessages() {
     try {
       await AsyncStorage.setItem('messages', JSON.stringify(this.state.messages));
@@ -69,30 +73,23 @@ export default class Chat extends React.Component {
       console.log(error.message);
     }
   }
-
-  async deleteMessages() {
-  try {
-    await AsyncStorage.removeItem('messages');
-    this.setState({
-      messages: []
-    })
-  } catch (error) {
-    console.log(error.message);
-  }
-}
   
-  // Hides toolbar when the user is offline
-  renderInputToolbar(props) {
-    if (this.state.isConnected == false) {
-    } else {
-      return <InputToolbar {...props} />;
+  async deleteMessages() {
+    try {
+      await AsyncStorage.removeItem('messages');
+      this.setState({
+        messages: []
+      })
+    } catch (error) {
+      console.log(error.message);
     }
   }
-
+  
+  
   componentDidMount() {
     // const name = this.props.route.params.username;
     // this.props.navigation.setOptions({ title: name });
-
+    
     // immediately checks if the user is online
     NetInfo.fetch().then((connection) => {
       if (connection.isConnected) {
@@ -101,93 +98,46 @@ export default class Chat extends React.Component {
         this.setState({
           isConnected: true,
         });
-
-    this.getMessages();
-    this.renderInputToolbar();
-
-    this.authUnsubscribe = firebase.auth().onAuthStateChanged((user) => {
-     if (!user) {
-       firebase.auth().signInAnonymously();
-     }
-     this.setState({
-       uid: user.uid,
-       messages: [],
-       user: {
-          _id: user.uid,
-          // name: name,
-          avatar: 'https://placeimg.com/140/140/any',
-        },
-     });
-
-     this.referenceChatUser = firebase
-      .firestore()
-      .collection("messages")
-      .where("uid", "==", this.state.uid);
-
-     this.unsubscribe = this.referenceChatMessages
-       .orderBy("createdAt", "desc")
-       .onSnapshot(this.onCollectionUpdate);
-    });
-  } else {
+        
+        this.getMessages();
+        this.renderInputToolbar();
+        
+        this.authUnsubscribe = firebase.auth().onAuthStateChanged((user) => {
+          if (!user) {
+            firebase.auth().signInAnonymously();
+          }
+          this.setState({
+            uid: user.uid,
+            messages: [],
+            user: {
+              _id: user.uid,
+              // name: name,
+              avatar: 'https://placeimg.com/140/140/any',
+            },
+          });
+          
+          this.referenceChatUser = firebase
+          .firestore()
+          .collection("messages")
+          .where("uid", "==", this.state.uid);
+          
+          this.unsubscribe = this.referenceChatMessages
+          .orderBy("createdAt", "desc")
+          .onSnapshot(this.onCollectionUpdate);
+        });
+      } else {
         // User is offline
         console.log('offline');
         this.setState({
           isConnected: false,
         }),
-
-          this.getMessages();
+        
+        this.getMessages();
       }
     });
   }
-
-
-
-  addMessage() {
-    const message = this.state.messages[0];
-    // saves data object to Firestore
-    this.referenceChatMessages.add({
-      uid: this.state.uid,
-      _id: message._id,
-      text: message.text,
-      createdAt: message.createdAt,
-      user: message.user,
-    });
-  }
-
-  onSend(messages = []) {
-   this.setState(previousState => ({
-    //  appends the new message to the messages object (to be displayed in the chat)
-     messages: GiftedChat.append(previousState.messages, messages),
-   }),
-      () => {
-        this.addMessage();
-        this.saveMessages();
-      }
-    );
-  }
-
-  // querySnapshot of all the data currently in collection
-  onCollectionUpdate = (querySnapshot) => {
-  const messages = [];
-  // each field within each document is saved to messages object, then rendered in app by setState() function
-  querySnapshot.forEach((doc) => {
-    // get the QueryDocumentSnapshot's data
-    var data = doc.data();
-    messages.push({
-        _id: data._id,
-        text: data.text,
-        createdAt: data.createdAt.toDate(),
-        user: {
-          _id: data.user._id,
-          name: data.user.name,
-        },
-    });
-  });
-  this.setState({
-      messages,
-    });
-};
-
+  
+  
   componentWillUnmount() {
     // stop authentication
     if (this.state.isConnected == false) {
@@ -197,10 +147,100 @@ export default class Chat extends React.Component {
       this.unsubscribe();
     }
   }
-
-  renderBubble(props) {
-    return (
-      <Bubble
+  
+  
+  
+  addMessage() {
+    const message = this.state.messages[0];
+    // saves data object to Firestore
+    this.referenceChatMessages.add({
+      uid: this.state.uid,
+      _id: message._id,
+      text: message.text,
+      createdAt: message.createdAt,
+      user: message.user,
+      avatar: 'https://placeimg.com/140/140/any',
+      // to send image/location
+      image: message.image || null,
+      location: message.location || null,
+    });
+  }
+  
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }), () => {
+      // Call the addMessage function with the message in the bubble as parameter
+      this.addMessage(messages[0])
+      // Save each sent message in AsyncStorage
+      this.saveMessages();
+    });
+  }
+  
+  
+  
+  // querySnapshot of all the data currently in collection
+  onCollectionUpdate = (querySnapshot) => {
+    const messages = [];
+    // each field within each document is saved to messages object, then rendered in app by setState() function
+    querySnapshot.forEach((doc) => {
+      // get the QueryDocumentSnapshot's data
+      let data = doc.data();
+      messages.push({
+        _id: data._id,
+        text: data.text,
+        createdAt: data.createdAt.toDate(),
+        image: data.image || null,
+        location: data.location || null,
+        user: {
+          _id: data.user._id,
+          name: data.user.name,
+        },
+      });
+    });
+    this.setState({
+      messages,
+      // Update the AsyncStorage with the latest changes in firebase
+    }, () => this.saveMessages());
+  };
+  
+  
+  // Hides toolbar when the user is offline
+  renderInputToolbar(props) {
+    if (this.state.isConnected == false) {
+    } else {
+      return <InputToolbar {...props} />;
+    }
+  }
+  
+  // shows action menu
+  renderCustomActions = (props) => {
+    return <CustomActions {...props} />;
+  };
+  
+  // returns MapView if message object contains location data
+  renderCustomView(props) {
+    const { currentMessage } = props;
+    if (currentMessage.location) {
+      return (
+        <MapView
+        style={{ width: 150, height: 100, borderRadius: 13, margin: 3 }}
+        region={{
+          latitude: currentMessage.location.latitude,
+          longitude: currentMessage.location.longitude,
+          latitudeDelta: 0.922,
+          longitudeDelta: 0.0421,
+        }}
+        />
+        );
+      }
+      return null;
+    }
+    
+    
+    renderBubble(props) {
+      return (
+        <Bubble
         // inherits props with ...props keyword
         {...props}
         wrapperStyle={{
@@ -208,40 +248,42 @@ export default class Chat extends React.Component {
             backgroundColor: '#000'
           }
         }}
-      />
-    )
-  }
-
-
-  render() {
-    let name = this.props.route.params.name; // OR ...
-    // let { name } = this.props.route.params;
-
-    this.props.navigation.setOptions({ title: name });
-
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: this.props.route.params.bgColor }}>
-        <View style={styles.chatWrapper}>
-            <GiftedChat
-                messages={this.state.messages}
-                renderBubble={this.renderBubble.bind(this)}
-                messages={this.state.messages}
-                renderInputToolbar={this.renderInputToolbar.bind(this)}
-                onSend={messages => this.onSend(messages)}
-                user={this.state.user}
-                onLongPress={() => this.deleteMessages()}
-              />
-            {/* prevents keyboard from covering messages in input field (for older android devices)  */}
-            {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
-            </View>
-        </View>
-    );
-  };
-}
-
-const styles = StyleSheet.create({
-  chatWrapper: {
-    flex: 1,
-    width: '100%'
-  },
-})
+        />
+        )
+      }
+      
+      
+      render() {
+        let name = this.props.route.params.name; // OR ...
+        // let { name } = this.props.route.params;
+        
+        this.props.navigation.setOptions({ title: name });
+        
+        return (
+          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: this.props.route.params.bgColor }}>
+          <View style={styles.chatWrapper}>
+          <GiftedChat
+          messages={this.state.messages}
+          renderBubble={this.renderBubble.bind(this)}
+          messages={this.state.messages}
+          renderInputToolbar={this.renderInputToolbar.bind(this)}
+          renderActions={this.renderCustomActions}
+          renderCustomView={this.renderCustomView}
+          onSend={messages => this.onSend(messages)}
+          user={this.state.user}
+          onLongPress={() => this.deleteMessages()}
+          />
+          {/* prevents keyboard from covering messages in input field (for older android devices)  */}
+          {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
+          </View>
+          </View>
+          );
+        };
+      }
+      
+      const styles = StyleSheet.create({
+        chatWrapper: {
+          flex: 1,
+          width: '100%'
+        },
+      })
---
+++ components/CustomActions.js
@@ -0,0 +1,186 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+import { Camera } from "expo-camera";
+import * as Permissions from 'expo-permissions';
+import * as ImagePicker from 'expo-image-picker';
+import * as Location from 'expo-location';
+
+import firebase from 'firebase';
+import firestore from 'firebase';
+
+export default class CustomActions extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: null,
+    };
+  }
+  
+  // Choose image from device library
+  pickImage = async () => {
+    const {status} = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    try {
+      if (status === 'granted') {
+        let result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        }).catch(error => console.log(error));
+        
+        if (!result.cancelled) {
+          const imageUrl = await this.uploadImageFetch(result.uri);
+          this.props.onSend({image: imageUrl});
+        }
+      }
+    } catch (error) { console.log(error.message) }
+  }
+
+  
+  // // Lets the user pick an image from the device's image library
+  // imagePicker = async () => {
+  //   // expo permission
+  //   const { status } = await Camera.requestPermissionsAsync();
+  //   try {
+  //     if (status === "granted") {
+  //       // pick image
+  //       const result = await ImagePicker.launchImageLibraryAsync({
+  //         mediaTypes: ImagePicker.MediaTypeOptions.Images, // only images are allowed
+  //       }).catch((error) => console.log(error));
+  //       // canceled process
+  //       if (!result.cancelled) {
+  //         const imageUrl = await this.uploadImageFetch(result.uri);
+  //         this.props.onSend({ image: imageUrl });
+  //       }
+  //     }
+  //   } catch (error) {
+  //     console.log(error.message);
+  //   }
+  // };
+  
+
+  // Lets the user take a photo with device's camera
+  takePhoto = async () => {
+    const {status} = await Permissions.askAsync(Permissions.CAMERA_ROLL, Permissions.CAMERA);
+    try {
+      if (status === 'granted') {
+        const result = await ImagePicker.launchCameraAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        }).catch(error => console.log(error));
+        
+        if (!result.cancelled) {
+          const imageUrl = await this.uploadImageFetch(result.uri);
+          this.props.onSend({image: imageUrl});
+        }
+      }
+    } catch (error) { console.log(error.message) }
+  }
+  
+  // Get the location of the user by using GPS
+  getLocation = async () => {
+    const {status} = await Permissions.askAsync(Permissions.LOCATION);
+    try {
+      if (status === 'granted') {
+        const result = await Location.getCurrentPositionAsync({})
+        .catch(error => console.log(error));
+        
+        const longitude = JSON.stringify(result.coords.longitude);
+        const latitude = JSON.stringify(result.coords.latitude)
+        if (result) {
+          this.props.onSend({
+            location: {
+              longitude: result.coords.longitude,
+              latitude: result.coords.latitude,
+            },
+          })
+        }
+      }
+    } catch (error) { console.log(error.message) }
+  }
+  
+  //Upload picture to firebse cloud storage
+  uploadImageFetch = async (uri) => {
+    const blob = await new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.onload = function() {
+        resolve(xhr.response);
+      };
+      xhr.onerror = function(e) {
+        console.log(e);
+        reject(new TypeError("Network request failed"));
+      };
+      xhr.responseType = "blob";
+      xhr.open("GET", uri, true);
+      xhr.send(null);
+    });
+    const imageNameBefore = uri.split("/");
+    const imageName = imageNameBefore[imageNameBefore.length - 1];
+    
+    const ref = firebase.storage().ref().child(`images/${imageName}`);
+    const snapshot = await ref.put(blob);
+    blob.close();
+
+    // gets the image url from storage
+    return await snapshot.ref.getDownloadURL();
+  }
+  
+  onActionPress = () => {
+    const options = ['Choose From Library', 'Take Picture', 'Send Location', 'Cancel'];
+    const cancelButtonIndex = options.length - 1;
+    this.context.actionSheet().showActionSheetWithOptions(
+      {
+        options,
+        cancelButtonIndex,
+      },
+      async (buttonIndex) => {
+        switch (buttonIndex) {
+          case 0:
+          console.log("user wants to pick an image");
+          return this.pickImage();
+          case 1:
+          console.log("user wants to take a photo");
+          return this.takePhoto();
+          case 2:
+          console.log("user wants to get their location");
+          return this.getLocation();
+        }
+      },
+      );
+    };
+    
+    render() {
+      return (
+        <TouchableOpacity style={[styles.container]} onPress={this.onActionPress}>
+        <View style={[styles.wrapper, this.props.wrapperStyle]}>
+        <Text style={[styles.iconText, this.props.iconTextStyle]}>+</Text>
+        </View>
+        </TouchableOpacity>
+        );
+      }
+    }
+    
+    const styles = StyleSheet.create({
+      container: {
+        width: 26,
+        height: 26,
+        marginLeft: 10,
+        marginBottom: 10,
+      },
+      wrapper: {
+        borderRadius: 13,
+        borderColor: '#b2b2b2',
+        borderWidth: 2,
+        flex: 1,
+      },
+      iconText: {
+        color: '#b2b2b2',
+        fontWeight: 'bold',
+        fontSize: 16,
+        backgroundColor: 'transparent',
+        textAlign: 'center',
+      },
+    });
+    
+    // defines actionSheet as a function so that you can use this.context.actionSheet().showActionSheetWithOptions in your onActionPress function
+    CustomActions.contextTypes = {
+      actionSheet: PropTypes.func,
+    };
